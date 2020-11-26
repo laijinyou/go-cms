aec5ccf4be0480f33b46ebe03547d12336014f71
Sloth <1183820@qq.com>
2020-11-26 09:40:07 +0900

    Add about and 404 pages.

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello World!This is a go website.</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello World!This is a go website.</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "Welcome to the about page."+"<a href=\"https://trip123.com\">Trip123.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>The request page is not found.</h1>"+"<p>if you have any questions,please contact me.</p>")
+	}
 }
 
 func main() {
