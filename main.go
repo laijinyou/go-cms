9d3a11ba48a1295fcd9ad848dd6c7170b8de619b
Sloth <1183820@qq.com>
2020-11-26 11:03:41 +0900

    try to use http.ServeMux

--- main.go
+++ main.go
@@ -9,15 +9,21 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello World!</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "Welcome to the about page."+"<a href=\"https://trip123.com\">Trip123.com</a>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>The request page is not found.</h1>"+"<p>if you have any questions,please contact me.</p>")
 	}
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "Welcome to the about page.Please click "+"<a href=\"https://trip123.com\">Trip123.com</a>")
+}
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", handlerFunc)
+	router.HandleFunc("/about", aboutHandler)
+
+	http.ListenAndServe(":3000", router)
 }
