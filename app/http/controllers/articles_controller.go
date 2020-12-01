8718a2ab8ba76f0436029fe80da6cf867a518f07
Sloth <1183820@qq.com>
2020-12-01 14:22:38 +0900

    rebuild articles list

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -9,7 +9,6 @@ import (
 	"net/http"
 	"text/template"
 
-	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
 
@@ -20,8 +19,7 @@ type ArticlesController struct {
 // Show 文章详情页面
 func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 	// 1. 获取 URL 参数
-	vars := mux.Vars(r)
-	id := vars["id"]
+	id := route.GetRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
 	article, err := article.Get(id)
@@ -48,5 +46,26 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 			ParseFiles("resources/views/articles/show.gohtml")
 		logger.LogError(err)
 		tmpl.Execute(w, article)
+	}
+}
+
+// Index 文章列表页
+func (*ArticlesController) Index(w http.ResponseWriter, r *http.Request) {
+
+	// 1. 获取结果集
+	articles, err := article.GetAll()
+
+	if err != nil {
+		// 数据库错误
+		logger.LogError(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "500 服务器内部错误")
+	} else {
+		// 2. 加载模板
+		tmpl, err := template.ParseFiles("resources/views/articles/index.gohtml")
+		logger.LogError(err)
+
+		// 3. 渲染模板，将所有文章的数据传输进去
+		tmpl.Execute(w, articles)
 	}
 }
--- bootstrap/db.go
+++ bootstrap/db.go
@@ -1,9 +1,37 @@
 package bootstrap
 
 import (
+	"goblog/pkg/logger"
 	"goblog/pkg/model"
 	"time"
+
+	// GORM 的 MSYQL 数据库驱动导入
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	gormlogger "gorm.io/gorm/logger"
 )
+
+// DB gorm.DB 对象
+var DB *gorm.DB
+
+// ConnectDB 初始化模型
+func ConnectDB() *gorm.DB {
+
+	var err error
+
+	config := mysql.New(mysql.Config{
+		DSN: "goblog:goblog123456@tcp(192.168.1.7:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+	})
+
+	// 准备数据库连接池
+	DB, err = gorm.Open(config, &gorm.Config{
+		Logger: gormlogger.Default.LogMode(gormlogger.Warn),
+	})
+
+	logger.LogError(err)
+
+	return DB
+}
 
 // SetupDB 初始化数据库和 ORM
 func SetupDB() {
