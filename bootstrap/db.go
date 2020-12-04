2d0909db57b0e30c292c36cb012b750dd464dacd
Sloth <1183820@qq.com>
2020-12-04 11:38:26 +0900

    create user

--- bootstrap/db.go
+++ bootstrap/db.go
@@ -1,37 +1,13 @@
 package bootstrap
 
 import (
-	"goblog/pkg/logger"
+	"goblog/app/models/article"
+	"goblog/app/models/user"
 	"goblog/pkg/model"
 	"time"
 
-	// GORM 的 MSYQL 数据库驱动导入
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	gormlogger "gorm.io/gorm/logger"
 )
-
-// DB gorm.DB 对象
-var DB *gorm.DB
-
-// ConnectDB 初始化模型
-func ConnectDB() *gorm.DB {
-
-	var err error
-
-	config := mysql.New(mysql.Config{
-		DSN: "goblog:goblog123456@tcp(192.168.1.7:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
-	})
-
-	// 准备数据库连接池
-	DB, err = gorm.Open(config, &gorm.Config{
-		Logger: gormlogger.Default.LogMode(gormlogger.Warn),
-	})
-
-	logger.LogError(err)
-
-	return DB
-}
 
 // SetupDB 初始化数据库和 ORM
 func SetupDB() {
@@ -48,4 +24,15 @@ func SetupDB() {
 	sqlDB.SetMaxIdleConns(25)
 	// 设置每个链接的过期时间
 	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+	// 创建和维护数据表结构
+	migration(db)
+}
+
+func migration(db *gorm.DB) {
+
+	// 自动迁移
+	db.AutoMigrate(
+		&user.User{},
+		&article.Article{},
+	)
 }
